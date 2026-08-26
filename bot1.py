2368b2d4643fdfdaebfd519bb12d9dad82a1ec02
YanYan <yaunyaun2026@gmail.com>
2026-08-27 01:26:54 +0630

    Update bot1.py

--- bot1.py
+++ bot1.py
@@ -716,7 +716,7 @@ def start(message):
     bot.send_message(
         message.chat.id, 
         
-        "<tg-emoji emoji-id='5251299553239398548'>🤖</tg-emoji> <b>မင်းတို့ကြမ်းတာထုပ်ထားတယ်             𝐏ᴏᴡᴇʀ 𝐁ʏ✿︎ -  @LeyoxYan_Yan !</b>\n\n"
+        "<tg-emoji emoji-id='5251299553239398548'>🤖</tg-emoji> <b> 𝐏ᴏᴡᴇʀ 𝐁ʏ✿︎ -  @LeyoxYan_Yan !</b>\n\n"
         "<tg-emoji emoji-id='5240241223632954241'>🚫</tg-emoji> Bio / Join / Link spam auto delete\n"
         "<tg-emoji emoji-id='6271786398404055377'>⚠️</tg-emoji> 3 Warnings = Auto Mute\n\n"
         "<tg-emoji emoji-id='5226945370684140473'>➕</tg-emoji> Bot ကို Group ထဲထည့်ပြီး Admin ပေးထားပါ။<tg-emoji emoji-id='5226945370684140473'>➕</tg-emoji> ",
