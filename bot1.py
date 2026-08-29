010260de851cc68b59d431dfe344ef3e1e1ddf69
YanYan <yaunyaun2026@gmail.com>
2026-08-29 22:19:41 +0630

    Dev999

--- bot1.py
+++ bot1.py
@@ -8,7 +8,7 @@ import os
 import pymongo  
 import random
 import html
-from program.enums import ButtonStyle
+from pyrogram.enums import ButtonStyle
 from telebot.types import ChatPermissions, InlineKeyboardMarkup, InlineKeyboardButton
 from dotenv import load_dotenv 
 
