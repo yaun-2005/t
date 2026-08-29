77aa66471335c33ea3c65ca7b2dec0d0eafa72de
YanYan <yaunyaun2026@gmail.com>
2026-08-29 17:43:01 +0630

    Dev999

--- bot1.py
+++ bot1.py
@@ -718,7 +718,7 @@ def start(message):
         
        
     
-        "<tg-emoji tg-emoji='5251299553239398548'>🤖</tg-emoji> <commandsင်းတို့ကြမ်းတာထုပ်ထားတယ်>မင်းတို့ကြမ်းတာထုပ်ထားတယ်!</b>\n\n"
+        "<tg-emoji tg-emoji='5251299553239398548'>🤖</tg-emoji> မင်းတို့ကြမ်းတာထုပ်ထားတယ်>မင်းတို့ကြမ်းတာထုပ်ထားတယ်!\n\n"
         "<tg-emoji emoji-id='5240241223632954241'>🚫</tg-emoji> Bio / Join / Link spam auto delete\n"
         "<tg-emoji emoji-id='6271786398404055377'>⚠️</tg-emoji> 3 Warnings = Auto Mute\n\n"
         "<tg-emoji emoji-id='5226945370684140473'>➕</tg-emoji> Bot ကို Group ထဲထည့်ပြီး Admin ပေးထားပါ။<tg-emoji emoji-id='5226945370684140473'>➕</tg-emoji> ",
